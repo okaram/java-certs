593d5bc1f39b5c2f73776c7913318a14b53bfb86
Orlando A Karam <orlando.karam@gmail.com>
2025-11-16 11:02:55 -0500

    more linked lists

--- associate_cert/com/okaram/samples/ds/LinkedList.java
+++ associate_cert/com/okaram/samples/ds/LinkedList.java
@@ -1,25 +1,80 @@
 package com.okaram.samples.ds;
 
+import java.security.InvalidParameterException;
+
 public class LinkedList {
-    static class Node {
+    private static class Node {
         int value;
         Node next;
+        Node prev;
+
+        public Node(int value, Node next,Node prev) {
+            this.value=value;
+            this.next=next;
+            this.prev=prev;
+        }
 
         public Node(int value, Node next) {
             this.value=value;
             this.next=next;
+            this.prev=null;
         }
+
         public Node(int value) {
             this.value=value;
             this.next=null;
+            this.prev=null;
         }
     };
 
     Node head=null;
+    Node last=null;
 
     // adds at the *beginning* of the list
     public void push(int value){
         head=new Node(value,head);
+        if (head.next!=null)
+            head.next.prev=head;
+        else 
+            last=head;
+    }
+
+    public void pop(){
+        if (head==null)
+            throw new InvalidParameterException("List is already empty");
+        if (head.next==null) {
+            head=null;
+            last=null;
+        }
+        head.next.prev=null;
+        head=head.next;
+    }
+
+    // remove from the end is an exercise
+    
+    // add at the end
+    public void add(int value) {
+        if (head==null)
+            push(value);
+        else {
+            last.next=new Node(value,null,last);
+            last=last.next;
+        }
+    }
+
+    private int size( Node current ){
+        if (current==null)
+            return 0;
+        return 1+size(current.next);
+    }
+
+    public int size() {
+        return size(head);
+        // int count=0;
+        // for(Node current=head; current!=null; current=current.next){
+        //     count++;
+        // }
+        // return count;
     }
 
     @Override
@@ -27,13 +82,20 @@ public class LinkedList {
         StringBuilder sb=new StringBuilder();
         sb.append("[ ");
         // could we convert this into a for loop?
-        Node current=head;
-        while(current!=null){
+        // Node current=head;
+        // while(current!=null){
+        //     sb.append(current.value);
+        //     sb.append(", ");
+        //     current=current.next;
+        // }
+
+        for(Node current=head; current!=null; current=current.next){
             sb.append(current.value);
-            sb.append(", ");
-            current=current.next;
+            if (current.next!=null)               
+                sb.append(", ");
         }
-        sb.append("]");
+
+        sb.append(" ]");
         return sb.toString();
     }
 }
