c3d85223d51d442cbf062263bef869746d126abb
Orlando A Karam <orlando.karam@gmail.com>
2026-01-11 10:34:22 -0500

    bucket hash

--- associate_cert/com/okaram/samples/ds/HashTable.java
+++ associate_cert/com/okaram/samples/ds/HashTable.java
@@ -11,18 +11,39 @@ public class HashTable {
         }
     }
 
-    ArrayList<Entry>[] values=new ArrayList<Entry>[100];
+    ArrayList<ArrayList<Entry>> values=new ArrayList<ArrayList<Entry>>();
 
-    // public void set(int key, String value) {
-    //     int index = key % values.length;
-    //     values[index] = new ArrayList<Entry>();
-    //     values[index].add(new Entry(key, value));
-    // }
+    public HashTable(int size){ {
+        for(int i=0;i<size;i++){
+            values.add(new ArrayList<Entry>());
+        }
+    }
 
-    // public String get(int key) {
-    //     int index = key % values.length;
-    //     Entry entry = values[index];
-    //     return entry != null && entry.key==key ? entry.value : null;
-    // }
+    public HashTable(){ {
+        for(int i=0;i<100;i++){
+            values.add(new ArrayList<Entry>());
+        }
+    }
+
+    public void set(int key, String value) {
+        int index = key % values.size();
+        ArrayList<Entry> bucket = values.get(index);
+        for(Entry entry: bucket){
+            if(entry.key==key){
+                entry.value=value;
+                return;
+            }
+        }
+        bucket.add(new Entry(key, value));
+    }
+
+    public String get(int key) {
+        int index = key % values.size();
+        ArrayList<Entry> bucket = values.get(index);
+        for(Entry entry: bucket)
+            if(entry.key==key)
+                return entry.value; 
+        return null;
+    }
 
 }
