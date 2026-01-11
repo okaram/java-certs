9a9c28a75e0f7164b48c932205d67d0e1c9728d4
Orlando A Karam <orlando.karam@gmail.com>
2026-01-11 10:07:27 -0500

    hash

---
+++ associate_cert/com/okaram/samples/ds/HashTable.java
@@ -0,0 +1,22 @@
+package com.okaram.samples.ds;
+
+public class HashTable {
+    static class Entry {
+        int key; // easier to illustrate
+        String value;
+
+        public Entry(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    void set(int key, String value) {
+        // TODO
+    }
+
+    String get(int key) {
+        // TODO
+        return null;
+    }
+}
